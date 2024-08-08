8909f814bc902032b96f8f106bb9c9945aa431ab
Amruta Pawar <amrutapawar9.ap@gmail.com>
2024-08-08 19:23:09 +0530

    commit 2

--- app/src/main/java/com/example/quotes/screens/QuoteListItem.kt
+++ app/src/main/java/com/example/quotes/screens/QuoteListItem.kt
@@ -1,4 +1,128 @@
 package com.example.quotes.screens
 
-class QuoteListItem {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.quotes.R
+
+@Preview
+@Composable
+fun QuoteListItem() {
+    Card(
+        elevation = CardDefaults.cardElevation(4.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                imageVector = Icons.Filled.Close,
+                alignment = Alignment.TopStart,
+                contentDescription = "Quote",
+                modifier = Modifier
+                    .size(40.dp)
+                    .rotate(180F)
+                    .background(Color.Black)
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(
+                    text = "time is money",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .background(Color(0xFFEEEEEE))
+                        .fillMaxWidth(.4f)
+                        .height(1.dp)
+                )
+                Text(
+                    text = "some Author",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun QuoteDetails() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(4.dp),
+            modifier = Modifier.padding(32.dp).align(Alignment.Center)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.Favorite,
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180F)
+                )
+                Text(
+                    text = "time is money",
+                    fontFamily = FontFamily(Font(R.font.myfont)),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Author",
+                    fontFamily = FontFamily(Font(R.font.myfont)),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+
+    }
+
+
 }
